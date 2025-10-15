6c513966db9cf5b0a0aa3b8ca84c52c314fa8158
kazushi-eguchi <sandwhale1010@gmail.com>
2025-10-15 17:43:30 +0900

    feat: Google Search Console認証用メタタグを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="r07cEeFNwbsmNtIFPFXttncKiGVqIYRDzR7z4g3avac" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
